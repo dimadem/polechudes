4199cfa7c85d06f067b587421539f7acad260b7c
dimadem <iamdimadem@gmail.com>
2025-06-06 14:35:40 +0100

    fix: backend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Crossword API", version="1.0.0")
 # CORS настройки
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],  # Vite dev server
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174", "https://polechudes.vercel.app"],  # Vite dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
